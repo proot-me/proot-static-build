d4c66a05e0f0d865ecc1fade11cd1c040cd9ce3c
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2017-09-08 21:32:17 +0100

    fix: simplify user management

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@
 
 DOCKER_IMAGE='proot/proot-buildenv:latest'
 
-CONTAINER_USERNAME='dummy'
-CONTAINER_GROUPNAME='dummy'
 GROUP_ID=$(id -g)
 USER_ID=$(id -u)
 
@@ -22,18 +20,6 @@ VOLUMES=${VOLUMES:-"${PROOT_LATEST}:/opt/build/packages/proot-latest.tar.gz;${CA
 
 ### FUNCTIONS
 
-create_user_cmd()
-{
-  if [[ ${USER_ID} -ne 0 ]]; then
-    echo \
-      groupadd -f -g ${GROUP_ID} ${CONTAINER_GROUPNAME} '&&' \
-      useradd -u ${USER_ID} -g ${GROUP_ID} ${CONTAINER_USERNAME} '&&' \
-      chown -R ${USER_ID}:${GROUP_ID} /opt/build
-  else
-    echo echo 'Running build as root...'
-  fi
-}
-
 mount_volumes() {
 
   if [[ -n ${VOLUMES} ]]; then
@@ -42,30 +28,17 @@ mount_volumes() {
 
 }
 
-execute_as_cmd()
-{
-  if [[ ${USER_ID} -ne 0 ]]; then
-    echo \
-      su ${CONTAINER_USERNAME} -c
-  else
-    echo \
-      eval
-  fi
-}
-
-full_container_cmd()
-{
-  echo "'$(create_user_cmd) && $(execute_as_cmd) \"$@\"'"
-}
-
 ### MAIN
 
 mkdir -p ${TARGET_DIR}
 eval docker run \
+    --user="${USER_ID}:${GROUP_ID}" \
+    -it \
     --rm=true \
     -a stdout \
     -v ${TARGET_DIR}:/opt/build/target $(mount_volumes) \
     -w /opt/build \
+    -e "PWD=/opt/build" \
     ${DOCKER_IMAGE} \
-    /bin/bash -ci $(full_container_cmd $@)
+    $@
 
